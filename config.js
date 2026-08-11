2148943c28908ff55b57372e2565b6eb7c0f4178
RReinsons <77511234+RReinsons@users.noreply.github.com>
2026-08-11 14:18:32 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.VM_CONFIG = {
-  apiUrl: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec",
+  apiUrl: "https://script.google.com/macros/s/AKfycbwyZXo31GHoRAHhwpWidogi2KuAEQdLBwt6AaFKJSeoIhr-uUDcnJSHUMNjIA_wAEYDmw/exec",
   pageSize: 12
 };
